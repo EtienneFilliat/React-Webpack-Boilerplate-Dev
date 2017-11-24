2183e1094d453d8ea5f88be60b49d9c11c4ef9eb
Etienne <etienne.filliat@epitech.eu>
2017-11-24 10:40:41 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: [
+    './app.js',
+    // './less/base.less',
+  ],
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      }
+    ]
+  },
+  devServer: {
+  contentBase: './www/',
+  inline: false,
+}
+}
